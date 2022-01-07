0e4b5e8849b308d98092d1be268c075b0cb0517a
suisuy <sadyn@qq.com>
2022-01-07 14:33:53 +0800

    test pointer event

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ let b2=document.querySelector('#b2');
 
 
 
-document.body.addEventListener('click',(e)=>{
-    console.log('body clicked')
-})
+
 
 d1.addEventListener('click',(e)=>{
     console.log('d1 clicked')
@@ -21,14 +19,59 @@ p1.addEventListener('click',(e)=>{
     console.log('p1 clicked')
 })
 
-b1.addEventListener('click',(e)=>{
-    console.log('b1 clicked')
-})
 
 d2.addEventListener('click',(e)=>{
+    e.stopPropagation();
     console.log('d2 clicked')
+})
+d2.addEventListener('click',(e)=>{
+    console.log('d2 clicked again')
+})
+
+b1.addEventListener('click',(e)=>{
+    e.stopImmediatePropagation();
+    console.log('b1 clicked')
+})
+b1.addEventListener('click',(e)=>{
+    console.log('b1 clicked again')
 })
 
 b2.addEventListener('click',(e)=>{
     console.log('b2 clicked')
 })
+
+
+
+//all pointerevent
+b2.addEventListener('pointerdown',(e)=>{
+    console.log('pointerdown')
+})
+b2.addEventListener('pointerup',(e)=>{
+    console.log('pointerup')
+})
+b2.addEventListener('pointerout',(e)=>{
+    console.log('pointerout')
+})
+b2.addEventListener('pointermove',(e)=>{
+    console.log('pointermove')
+})
+b2.addEventListener('pointerover',(e)=>{
+    console.log('pointerover')
+})
+b2.addEventListener('pointerleave',(e)=>{
+    console.log('pointerleave')
+})
+b2.addEventListener('pointercancel',(e)=>{
+    console.log('pointercancel')
+})
+b2.addEventListener('pointerenter',(e)=>{
+    console.log('pointerenter')
+})
+b2.addEventListener('gotpointercapture',(e)=>{
+    console.log('gotpointcapture')
+})
+b2.addEventListener('leavepointercapture',(e)=>{
+    console.log('leavepointcapture')
+})
+
+
